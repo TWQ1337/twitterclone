e9ed650254b382d25eb61731dbdcf769d9428e84
twq1337 <TWERQEN@ya.ru>
2022-07-10 17:31:49 -0700

    added links to profiles on feed page

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -5,13 +5,18 @@ from .models import TweetModel
 
 class TweetSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField(read_only=True)
+    user_id = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = TweetModel
         fields = [
             'user',
+            'user_id',
             'body',
             'timestamp'
         ]
 
     def get_user(self, obj):
         return obj.created_by.username
+
+    def get_user_id(self, obj):
+        return obj.created_by.id
